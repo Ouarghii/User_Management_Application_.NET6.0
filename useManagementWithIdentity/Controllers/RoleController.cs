09fefc49833a7216f8f186182b2141429d598eb4
Ouarghii <Raslenouarghi2018@gmail.com>
2024-01-01 14:00:36 +0100

    updated

--- useManagementWithIdentity/Controllers/RoleController.cs
+++ useManagementWithIdentity/Controllers/RoleController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using useManagementWithIdentity.Models;
+using useManagementWithIdentity.ViewModels;
 
 [Authorize(Roles = "Admin")]
 public class RoleController : Controller
@@ -16,6 +17,45 @@ public class RoleController : Controller
 
     public async Task<IActionResult> Index()
     {
+        var roles = await _roleManager.Roles.ToListAsync();
+        return View("~/Views/Roles/Index.cshtml", roles);
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Add(RoleFormViewModel model)
+    {
+        if (ModelState.IsValid)
+        {
+            var roleExists = await _roleManager.RoleExistsAsync(model.Name);
+
+            if (roleExists)
+            {
+                ModelState.AddModelError("Name", "Role already exists!");
+            }
+            else
+            {
+                // Trim the role name before creating
+                var role = new IdentityRole(model.Name.Trim());
+
+                var result = await _roleManager.CreateAsync(role);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+                else
+                {
+                    // Handle the error, e.g., add errors to ModelState
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+                }
+            }
+        }
+
+        // If we reach here, there was a validation error or role already exists; return to the form
         var roles = await _roleManager.Roles.ToListAsync();
         return View("~/Views/Roles/Index.cshtml", roles);
     }
---
+++ useManagementWithIdentity/Controllers/UsersController.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using useManagementWithIdentity.Models;
+using useManagementWithIdentity.ViewModels;
+
+namespace useManagementWithIdentity.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class UsersController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public UsersController(UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+
+        }
+        public async Task<IActionResult> Index()
+        {
+            var users=await _userManager.Users.Select(user=>new UserViewModel
+            {
+                id= user.Id,
+                Firstname= user.FirstName,
+                Lastname= user.LastName,
+                Email= user.Email,
+                Username= user.UserName,
+                Roles =_userManager.GetRolesAsync(user).Result
+            }).ToListAsync();
+            return View(users);
+        }
+        public async Task<IActionResult> ManageRoles(string userId)
+        {
+            var user=await _userManager.FindByIdAsync(userId);
+            if(user==null)
+                return NotFound();
+            var roles = await _roleManager.Roles.ToListAsync();
+            var viewModel = new UserRolesViewModel
+            {
+                UserId = user.Id,
+                UserName = user.FirstName,
+                Roles = roles.Select(async role => new RoleViewModel
+                {
+                    RoleId = role.Id,
+                    RoleName = role.Name,
+                    IsSelected =await _userManager.IsInRoleAsync(user, role.Name)
+                }).Select(Task=>Task.Result)
+                .ToList(),
+            };
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> ManageRoles(UserRolesViewModel model)
+        {
+            var user = await _userManager.FindByIdAsync(model.UserId);
+            if (user == null)
+                return NotFound();
+
+            var userRoles = await _userManager.GetRolesAsync(user);
+             
+            foreach(var role in model.Roles) {
+                if(userRoles.Any(r=>r==role.RoleName) && !role.IsSelected)
+                {
+                    await _userManager.RemoveFromRoleAsync(user, role.RoleName);
+                }
+                if (userRoles.Any(r => r == role.RoleName) && role.IsSelected)
+                {
+                    await _userManager.AddToRoleAsync(user, role.RoleName);
+                }
+
+
+            } return RedirectToAction(nameof(Index));
+        }
+    }
+}
